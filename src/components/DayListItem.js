0d5d3f54706b1e11bb44366a9fc565cd4647c1b4
Anneloes <anneloes9405@hotmail.com>
2020-02-17 21:58:36 -0500

    removed weird extra dependencies, created DayList that renders days with spots remaining

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,12 +1,27 @@
 import React from "react";
 import "./DayListItem.scss";
+import classNames from "classnames"
 
+function formatSpots (spots) {
+  if (spots == 0) {
+    return "no spots"
+  } else if (spots == 1) {
+    return "1 spot"
+  } else {
+    return spots + " spots"
+  }
+}
 
 export default function DayListItem(props) {
+  const dayClass = classNames({
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0
+  })
+
   return (
     <li onClick={() => props.setDay(props.name)}>
-      <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots}</h3>
+      <h2 className={dayClass}>{props.name}</h2> 
+      <h3 className={dayClass}>{formatSpots(props.spots)} remaining</h3>
     </li>
   );
 }
