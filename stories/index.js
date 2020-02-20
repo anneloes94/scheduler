614c7a1cd6a6e56b1983406568b5f1056c387ab7
Anneloes <anneloes9405@hotmail.com>
2020-02-19 19:19:43 -0500

    created multiple Appointment components

--- stories/index.js
+++ stories/index.js
@@ -13,6 +13,11 @@ import InterviewerList from "components/InterviewerList";
 
 import Appointment from "components/Appointment/index"
 import Header from "components/Appointment/Header"
+import Empty from "components/Appointment/Empty"
+import Show from "components/Appointment/Show"
+import Confirm from "components/Appointment/Confirm"
+import Status from "components/Appointment/Status"
+import Error from "components/Appointment/Error"
 
 storiesOf("Button", module)
   .addParameters({
@@ -133,15 +138,11 @@ storiesOf("DayListItem", module) //Initiates Storybook and registers our DayList
     .addParameters({
       backgrounds: [{ name: "white", value: "#fff", default: true }]
     })
-    .add("Appointment", () => <Appointment />)
-    .add("Appointment with time", () => {
-      <Appointment time="12PM"/>
-    })
-    .add("Header", () => {
-      <Header time="12pm" />
-    })
-  
-  
-
-
-
+    .add("Appointment", () => (<Appointment />))
+    .add("Appointment with time", () => (<Appointment time="12PM"/>))
+    .add("Header", () => (<Header time="12pm" /> ))
+    .add("Empty", () => (<Empty onAdd={action("onAdd")} />))
+    .add("Show", () => (<Show onEdit={action("onEdit")} onDelete={action("onDelete")}/>))
+    .add("Confirm", () => (<Confirm message="Delete the appointment?" onConfirm={action("onConfirm")} onCancel={action("onCancel")} />))
+    .add("Status", () => (<Status message="Deleting" />))
+    .add("Error", () => (<Error onClose={action("onClose")} message="Could not delete appointment" />))
