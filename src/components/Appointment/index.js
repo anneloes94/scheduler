bbbdd0a59f88949af324f8b66a71477aca7b3a1b
Anneloes <anneloes9405@hotmail.com>
2020-02-23 21:21:29 -0500

    created conditionally rendering component in modes EMPTY, SHOW, and CREATE

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,14 +3,37 @@ import "./styles.scss"
 import Header from "./Header"
 import Show from "./Show"
 import Empty from "./Empty"
+import useVisualMode from "../../hooks/useVisualMode"
 
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
+const CREATE = "CREATE"
+props.interviewers = []
 
 export default function Appointment(props) {
-  return <article className="appointment" 
-  key={props.id}>
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
+  
+  return (
+  <article className="appointment" key={props.id}>
     <Header time={props.time}> </Header>
+
+    {mode === EMPTY &&
+      <Empty onAdd={() => transition(CREATE)} />}
+
+    {mode === CREATE &&
+      <Form
+        name={}
+        interviewer={}
+        onCancel={() => back()}
+        interviewers={props.interviewers}
+        onSave={save}
+        isSave={true}
+      />
+    }
     {props.interview ? <Show interviewer={props.interviewer} student={props.student}></Show> : <Empty></Empty>}
-    </article>
+  </article>
+  )
 }
