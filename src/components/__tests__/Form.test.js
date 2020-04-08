c7388db0d59975ed5d3064f66711629b45931315
Anneloes <anneloes9405@hotmail.com>
2020-04-08 14:36:33 -0400

    replaced two tests for one that does the same. toHaveBeenCalled still does not work

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -48,37 +48,28 @@ describe("Form", () => {
     expect(onSave).not.toHaveBeenCalled();
   });
   
-  it("calls onSave function when the name is defined", () => {
-        /* 1. Create the mock onSave function */
-        const onSave = jest.fn();
-
-        /* 2. Render the Form with interviewers and the onSave mock function passed as an onSave prop, the name prop should be blank or undefined */
-        const { getByText } = render(
-          <Form interviewers={interviewers} onSave={onSave} />
-        ); 
-    /* 3. validation is not shown */
-    expect(queryByText(/student name cannot be blank/i)).toBeNull();
-  
-    /* 4. onSave is called once*/
-    expect(onSave).toHaveBeenCalledTimes(1);
-  
-    /* 5. onSave is called with the correct arguments */
-    expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", null);
-  });
-  
-  it("submits the name entered by the user", () => {
+  it("can successfully save after trying to submit an empty student name", () => {
     const onSave = jest.fn();
-    const { getByText, getByPlaceholderText } = render(
+    const { getByText, getByPlaceholderText, queryByText } = render(
       <Form interviewers={interviewers} onSave={onSave} />
     );
   
-    const input = getByPlaceholderText("Enter Student Name");
-  
-    fireEvent.change(input, { target: { value: "Lydia Miller-Jones" } });
     fireEvent.click(getByText("Save"));
   
+    expect(getByText(/Student name cannot be blank/i)).toBeInTheDocument();
+    expect(onSave).not.toHaveBeenCalled();
+  
+    fireEvent.change(getByPlaceholderText("Enter Student Name"), {
+      target: { value: "Lydia Miller-Jones" }
+    });
+  
+    fireEvent.click(getByText("Save"));
+  
+    expect(queryByText(/Student name cannot be blank/i)).toBeNull();
+
     expect(onSave).toHaveBeenCalledTimes(1);
     expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", null);
   });
+  
   
 });
