92e1c62847a239e93b97800bb4f08ecd0f185b26
Anneloes <anneloes9405@hotmail.com>
2020-04-09 13:35:06 -0400

    cancel interview test setup

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -17,6 +17,7 @@ describe("Application", () => {
     })
   });
 
+
   it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
     const { container, debug } = render(<Application />);
     await waitForElement(() => getByText(container, "Archie Cohen"));
@@ -48,6 +49,27 @@ describe("Application", () => {
     );
 
     expect(getByText(day, "no spots remaining")).toBeInTheDocument();
-    
   })
+
+  it("loads data, cancels an interview and increases the spots remaining for Monday by 1", async () => {
+    // 1. Render the Application.
+    const { container, debug } = render(<Application />);
+
+    // 2. Wait until the text "Archie Cohen" is displayed.
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+
+    // 3. Click the "Delete" button on the booked appointment.
+    const appointments = getAllByTestId(container, "appointment");
+    const appointment = appointments[1];
+    console.log(prettyDOM(appointment))
+
+    fireEvent.click(getByAltText(appointment, "Delete"))
+
+    // 4. Check that the confirmation message is shown.
+    // 5. Click the "Confirm" button on the confirmation.
+    // 6. Check that the element with the text "Deleting" is displayed.
+    // 7. Wait until the element with the "Add" button is displayed.
+    // 8. Check that the DayListItem with the text "Monday" also has the text "2 spots remaining".
+  });
+  
 })
